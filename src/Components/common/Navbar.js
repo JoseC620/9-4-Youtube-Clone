b905304c59715d4dc82c4ec69455de4c30a1ea5c
USERNAME <EMAIL>
2023-03-11 22:55:56 -0500

    added youtube logo

--- src/Components/common/Navbar.js
+++ src/Components/common/Navbar.js
@@ -2,8 +2,16 @@ import { Link } from "react-router-dom"
 import "./Navbar.css"
 
 export default function Navbar() {
-    return (
+   
+  return (
+
         <header>
+
+            <div className="youtube-logo" style={{ marginRight: "10px" }}>
+              <div className="playBtn"></div>
+            </div>
+
+
         <article>
           <h1>
             <Link to="/">
