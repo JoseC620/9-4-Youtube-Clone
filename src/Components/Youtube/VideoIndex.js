90fe49a15fd0ce9e5192681e5304e163fd0ebdf5
JoseC620 <115428298+JoseC620@users.noreply.github.com>
2023-03-12 12:31:12 -0400

    Revert "GOT VIDEOS TO PLAY"

--- src/Components/Youtube/VideoIndex.js
+++ src/Components/Youtube/VideoIndex.js
@@ -2,14 +2,11 @@
 
 export default function VideoIndex() {
 
-let id = window.location.pathname.substring(8)
 
 
     return (
-        <div id="player">
-            <iframe src={`https://www.youtube.com/embed/${id}`}>
-            </iframe>
-           
+        <div>
+            
         </div>
     )
 }
