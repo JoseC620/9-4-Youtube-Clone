ade6dbb4b297ec3c2b4dc45f7cc6a4cef100d10e
USERNAME <EMAIL>
2023-03-12 11:50:26 -0400

    Changed background for Light Dark mode

--- src/Components/Youtube/VideoListing.js
+++ src/Components/Youtube/VideoListing.js
@@ -11,40 +11,36 @@ export default function VideoListing() {
 
     const [loadingError, setLoadingError] = useState(false)
     const [searchTerm, setSearchTerm] = useState("surfing")
-    const [videos, setVideos] = useState([])
+    const [videos, setVideos] = useState({})
 
-    useEffect(() => {
-        getAllVideos(searchTerm).then((response) => {
-          console.log(response.items)
-          setVideos(response.items)
-          if (Object.keys(response).length === 0) {
-            setLoadingError(true)
-          } else {
-            setLoadingError(false)
-          }
-        }).catch((error) => {
-          setLoadingError(true)
-        })
-      }, [searchTerm])
-
-
-    
-
+    // useEffect(() => {
+    //     getAllVideos(searchTerm).then((response) => {
+    //       console.log(response)
+    //       setVideos(response)
+    //       if (Object.keys(response).length === 0) {
+    //         setLoadingError(true)
+    //       } else {
+    //         setLoadingError(false)
+    //       }
+    //     }).catch((error) => {
+    //       setLoadingError(true)
+    //     })
+    //   }, [searchTerm])
 
 
     return (
         
         <div className="videos">
-          {videos.map((video) => {
+          {/* {videos.items.map((video) => {
             return (
               <div key={video.id.videoId}>
-                <Link to={`/Videos/${video.id.videoId}`}>
-                <img src={video.snippet.thumbnails.high.url} alt="box" width="230px" className="thumbnail"></img>
+                <Link>
+                <img src={video.snippet.thumbnails.default.url} alt="box" width="250px" className="thumbnail"></img>
                 <h3 className="text">{video.snippet.title}</h3>
                 </Link>
               </div>
             )
-          })}
+          })} */}
             
             </div>
     )
