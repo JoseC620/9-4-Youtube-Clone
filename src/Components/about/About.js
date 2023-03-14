33995c68f7dd840fac384dcd0886bfd015a884f6
USERNAME <EMAIL>
2023-03-13 21:40:56 -0400

    About page done

--- src/Components/about/About.js
+++ src/Components/about/About.js
@@ -11,13 +11,21 @@ export default function About() {
     const teamMembers = [
         {
             name: "Jose Cepeda",
-            bio: "Super fantastic guy",
+            bio: `I'm currently a Full Stack Web Development Fellow at Pursuit, a 12-month, Google-funded software engineering fellowship with a 9% acceptance rate whose graduates have been hired as developers at leading companies such as Pinterest, JP Morgan, Citi, Spotify, and Blackstone.
+    
+            I'm currently learning React, having already studied JavaScript, HTML & CSS and I am training to become a software engineer so that I can solve real-world problems.`,
             github: "https://github.com/JoseC620",
         },
 
         {
             name: "Mark Roberston",
-            bio: "Super fantastic guy",
+            bio: `I'm currently a Full Stack Web Development Fellow at Pursuit, a 12-month, Google-funded software engineering fellowship with a 9% acceptance rate whose graduates have been hired as developers at leading companies such as Pinterest, JP Morgan, Citi, Spotify, and Blackstone.
+    
+            I'm currently learning React, having already studied JavaScript, HTML & CSS and I am training to become a software engineer so that I can solve real-world problems.  When I'm not coding, I enjoy movies, watching Formula 1 racing, music, travel and playing with my German Shepherd.
+
+            Being a Pursuit Fellow will give me the foundation to go on and learn new and exciting languages such as Python, C++, C# et al. that I would like to use both personally and professionally. My first personal project will be to design and implement a quantitative trading algorithm that I can run through an API to a trading platform.
+            
+            I would very much enjoy working for a start-up (as I have start-up experience) or even a large financial firm, as my experience up until Pursuit has been in the financial markets.`,
             github: "https://github.com/MarkRobertson67",
         },
     ];
@@ -29,7 +37,7 @@ export default function About() {
           <div className="about">
             <h1>About Our Project</h1>
             <p>
-              Our project is a video search and viewing app that allows users to
+              Our project is a video search and viewing app, YouTube clone, that allows users to
               search for videos and play them within the app. The app has a simple
               and intuitive user interface, which includes a search bar on the home
               page that allows users to get a list of videos based on their search
@@ -52,7 +60,7 @@ export default function About() {
             </p>
             <p>
               Our app is built using JSX to create components, and we have committed
-              to using either class based components or hooks, but not both. We have
+              to using  hooks. We have
               used create-react-app to build the app and fetch to make API calls.
               React-router-dom is used to create different views, while youtube-react
               is used to create a Youtube component for viewing videos from the
@@ -61,9 +69,17 @@ export default function About() {
               interface.
             </p>
           </div>
+
+          <h1>App Screenshots</h1>
+          <div className="app-screenshots">
+          {/* <img src={process.env.PUBLIC_URL + '/searchscreen.png'} alt="Screenshota" style={{ width: '30%'}} /> */}
+            <img src="./homescreen.png" alt="Screenshota" style={{ width: '30%'}} />
+            <img src="./searchscreen.png" alt="Screenshotb" style={{ width: '30%'}} />
+            <img src="./videoscreen.png" alt="Screenshotc" style={{ width: '30%'}} />
+          </div>
     
-          <div className="team">
-            <h2>Meet the Team</h2>
+          <div className="team"><br></br><br></br>
+            <h1>Meet the Team</h1>
             <div className="team-members">
               {teamMembers.map((member, index) => (
                 <div key={index} className="member-card">
