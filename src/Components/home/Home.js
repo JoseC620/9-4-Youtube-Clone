8679a4f89ecb15a3ebe6e3fcf672049c3ecace8d
JoseC620 <Jcepeda6201@gmail.com>
2023-03-19 12:52:02 -0400

    fixed search bar and added modal

--- src/Components/home/Home.js
+++ src/Components/home/Home.js
@@ -3,10 +3,11 @@ import { useState } from "react"
 import { Link } from "react-router-dom";
 import './Home.css'
 
-export default function Home({ callback, mode, show }) {
+export default function Home({ callback, mode }) {
 
     const [searchVisible, setSearchVisible] = useState(false);
     const [searchTerm, setSearchTerm] = useState("")
+    const [loadingError, setLoadingError] = useState(false)
   
     let image
 
@@ -24,6 +25,12 @@ export default function Home({ callback, mode, show }) {
       const handleSearchInputChange = (event) => {
         setSearchTerm(event.target.value);
       };
+
+      function handleSearch(){
+        if(searchTerm === ""){
+          setLoadingError(true)
+        }
+      }
     
       const handleSearchFormSubmit = (e) => {
         e.preventDefault();
@@ -31,13 +38,19 @@ export default function Home({ callback, mode, show }) {
         setSearchVisible(false);
       };
 
-      
+      function pleaseSubmit() {
+        return(
+          <div>
+            <h3>Please submit a search!</h3>
+          </div>
+        )
+      }
 
 
       return (
-
-        
+       
         <div className="home-container" style={{backgroundImage: `url(${image})`}}>
+          
           <div className="search-container">
             <div className="search-icon" onClick={handleSearchIconClick} style={{backgroundColor: !mode ? "red": "blue"}}>
               <i className="fa fa-search"></i>
@@ -52,11 +65,14 @@ export default function Home({ callback, mode, show }) {
                 value={searchTerm}
                 onChange={handleSearchInputChange}
               />
-              <Link to={`/Videos/Search/${!searchTerm ? "": searchTerm}`}>
-              <button type="submit" onClick={() => callback(searchTerm)}>Go</button>
+              <Link to={!searchTerm ? `/error` : (`/Videos/Search/${searchTerm}`)}>
+              <button type="submit" onClick={() => {
+                handleSearch()
+                callback(searchTerm)}}>Go</button>
               </Link>
             </form>
           </div>
+          <section className="pleaseText">{searchVisible ? pleaseSubmit(): ""}</section>
         </div>
       )
 }
